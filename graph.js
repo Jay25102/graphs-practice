c9c63e005c0393cf1d041a274266576e750f1783
Jay25102 <jasonhuang25102@gmail.com>
2024-08-15 13:26:07 -0400

    dfs and bfs

--- graph.js
+++ graph.js
@@ -17,7 +17,7 @@ class Graph {
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
   addVertices(vertexArray) {
-    for (vertex of vertexArray) {
+    for (let vertex of vertexArray) {
       this.nodes.add(vertex);
     }
   }
@@ -30,14 +30,14 @@ class Graph {
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
   removeEdge(v1, v2) {
-    v1.adjacent.remove(v2);
-    v2.adjacent.remove(v1);
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
   }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
   removeVertex(vertex) {
     this.nodes.delete(vertex);
-    for (n in this.nodes) {
+    for (let n of this.nodes) {
       if (n.adjacent.has(vertex)) {
         n.adjacent.delete(vertex);
       }
@@ -45,10 +45,50 @@ class Graph {
   }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    let result = [];
+    let seen = new Set();
+    
+    function dfsTraverse(node) {
+      if (!node) {
+        return null;
+      }
+
+      seen.add(node);
+      result.push(node.value);
+
+      node.adjacent.forEach(neighbor => {
+        if (!seen.has(neighbor)) {
+          return dfsTraverse(neighbor);
+        }
+      })
+    }
+    dfsTraverse(start);
+
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    let result = [];
+    let queue = [start];
+    let seen = new Set();
+    let currNode;
+    seen.add(start);
+
+    while (queue.length) {
+      currNode = queue.shift();
+      result.push(currNode.value);
+
+      currNode.adjacent.forEach(neighbor => {
+        if (!seen.has(neighbor)) {
+          seen.add(neighbor);
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 module.exports = {Graph, Node}
